07c93d7379ed6a132759469007ad14d9543e49be
stanley_su@live.com <xun.su@mail.mcgill.ca>
2019-02-13 15:09:46 -0800

    Setup webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
