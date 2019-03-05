26d41ffa432940ff406ea2999d3ad7add49d5da3
stanley_su@live.com <xun.su@mail.mcgill.ca>
2019-03-05 15:40:55 -0500

    Added socket utility for updating moves

--- components/App.jsx
+++ components/App.jsx
@@ -16,14 +16,57 @@ class App extends Component{
 				row: -1,
 				col: -1,
 				val: 0
-			}
+			},
+			connected: false
 		};
 		this.toggleSelectedCell = this.toggleSelectedCell.bind(this);
 		this.handleKeyDown = this.handleKeyDown.bind(this);
+		this.addMove = this.addMove.bind(this);
 	}
 
 	componentDidMount(){
 		document.addEventListener("keydown", this.handleKeyDown);
+
+		let ws = this.ws = new WebSocket('ws://echo.websocket.org');
+		ws.onmessage = this.message.bind(this);
+		ws.onopen = this.open.bind(this);
+		ws.onclose = this.close.bind(this);
+	}
+
+	message(e){
+		const event = JSON.parse(e.data);		
+		if(event.name === 'move add'){
+			this.setMove(event.data)
+		}
+	}
+	open(){
+		this.setState({connected: true})
+	}
+	close(){
+		this.setState({connected: false})
+	}
+
+	addMove(move){
+		let msg = {
+			name: 'move add',
+			data: move
+		}
+
+		this.ws.send(JSON.stringify(msg))
+	}
+
+	setMove(move){
+		let {board, selectedCell} = this.state;
+
+		board[move.row][move.col].value = move.value;
+		this.setState({
+			board: board
+		});
+
+		selectedCell.val = move.value;
+		this.setState({
+			selectedCell: selectedCell
+		});
 	}
 
 	componentWillUnmount() {
@@ -56,25 +99,18 @@ class App extends Component{
 		if(this.state.selectedCell.isSelected) {
 			const keyCode = event.keyCode;
 			//check keycode between 1 to 9 on keyboard or numpad
-			if(isBetween1and9(keyCode) || isBackspaceOrDelete(keyCode)) { 
-				const newValue = isBetween1and9(keyCode) ? event.key : null;
+			if((isBetween1and9(keyCode) || isBackspaceOrDelete(keyCode)) &&  
+				this.state.board[this.state.selectedCell.row][this.state.selectedCell.col].editable){
+				const newValue = isBetween1and9(keyCode) ? parseInt(event.key) : null;
 
-				if(this.state.board[this.state.selectedCell.row][this.state.selectedCell.col].editable){
-					const newBoard = deepCopy(this.state.board);
-					newBoard[this.state.selectedCell.row][this.state.selectedCell.col].value = newValue;
-					this.setState({
-						board: newBoard
-					});
-
-					const newSelectedCell = deepCopy(this.state.selectedCell);
-					newSelectedCell.val = newValue;
-					this.setState({
-						selectedCell: newSelectedCell
-					});
+				const move = {
+					row: this.state.selectedCell.row,
+					col: this.state.selectedCell.col,
+					value: newValue
 				}
+				this.addMove(move);
 			}
 		}
-	 	
 	}
 
 	render(){
